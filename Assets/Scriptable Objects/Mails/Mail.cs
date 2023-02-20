89c7a79f91a169a00bcb62ed39bf25ae3efbeade
Evelyn <evelyn.romanjuk@gmail.com>
2023-02-20 12:45:37 +0100

    Scriptable Objects Mail, Mail Display
    - Imported scriptable object for mails and MailDisplay script from other project
    - set up the scene so mails can be displayed here as well
    - deleted the HackerButton prefab variants for messages, hacker3000 and robot, because they are not necessary

---
+++ Assets/Scriptable Objects/Mails/Mail.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Mail", menuName = "Mail")]
+public class Mail : ScriptableObject
+{
+    public string senderAdress;
+    public string receiverAdress;
+    public string subject;
+    public string date;
+    public string content;
+
+}
---
+++ Assets/Scripts/UI/MailDisplay.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class MailDisplay : MonoBehaviour
+{
+
+    public Mail mail;
+    private TMP_Text buttonSubject;
+    public Image contentImage;
+
+    private Button mailButton;
+    private GameObject contentImageText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        mailButton = this.GetComponent<Button>();
+        mailButton.onClick.AddListener(printMail);
+
+        buttonSubject = mailButton.transform.GetChild(0).GetComponent<TMP_Text>();
+        buttonSubject.text = mail.subject;
+
+        contentImageText = contentImage.transform.GetChild(0).gameObject;
+        
+    }
+
+    public void printMail()
+    {
+        if(!contentImageText.activeInHierarchy)
+        {
+            contentImageText.SetActive(true);
+        }
+
+        TMP_Text contentSubject = contentImageText.transform.GetChild(0).GetComponent<TMP_Text>();
+        TMP_Text contentDate = contentImageText.transform.GetChild(1).GetComponent<TMP_Text>();
+        TMP_Text contentFrom = contentImageText.transform.GetChild(2).GetComponent<TMP_Text>();
+        TMP_Text contentTo = contentImageText.transform.GetChild(3).GetComponent<TMP_Text>();
+        TMP_Text contentMail = contentImageText.transform.GetChild(4).GetComponent<TMP_Text>();
+
+        contentSubject.text = mail.subject;
+        contentDate.text = mail.date;
+        contentFrom.text = mail.senderAdress;
+        contentTo.text = mail.receiverAdress;
+        contentMail.text = mail.content;
+
+        Debug.Log("Mail Subject of Button clicked: " + mail.subject);
+    }
+}
