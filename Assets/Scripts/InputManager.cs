e104a864a56df39fe0ad5d0db4043d1ec2a7ebb3
Evelyn <evelyn.romanjuk@gmail.com>
2023-02-22 17:39:00 +0100

    First setup of Input System
    - imported input system
    - set up bindings from old project
    - pasted scripts from old project for movement and looking around

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    public PlayerInput.OnFootActions onFoot;
+
+    private PlayerMovement movement;
+    private PlayerLook look;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+
+        movement = GetComponent<PlayerMovement>();
+        look = GetComponent<PlayerLook>();
+
+        onFoot.Jump.performed += ctx => movement.Jump(); //any time onFoot.Jump is performed, we use a callback context to call movement.Jump
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //tell the PlayerMovement to move using the value from our movement action   
+        movement.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+
+    private void LateUpdate()
+    {
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
+    }
+
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+
+    private void OnDisable()
+    {
+        onFoot.Disable();
+    }
+}
