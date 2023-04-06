a2f0e91d95104fb30711b151e5abb0df7f5af4a3
Evelyn <evelyn.romanjuk@gmail.com>
2023-04-06 15:30:29 +0200

    Started adding button to Frame_LabMail

--- Assets/Scripts/UI/MailDisplay.cs
+++ Assets/Scripts/UI/MailDisplay.cs
@@ -9,10 +9,8 @@ public class MailDisplay : MonoBehaviour
 
     public Mail mail;
     private TMP_Text buttonSubject;
-    public Image contentImage;
 
     private Button mailButton;
-    //private GameObject contentImageText;
     public MailEntry MailEntry;
 
     // Start is called before the first frame update
@@ -24,32 +22,11 @@ public class MailDisplay : MonoBehaviour
 
         buttonSubject = mailButton.transform.GetChild(0).GetComponent<TMP_Text>();
         buttonSubject.text = mail.subject;
-
-        //contentImageText = contentImage.transform.GetChild(0).gameObject;
-        
     }
 
     public void printMail()
     {
         MailEntry.SetMailEntry(mail.subject, mail.date, mail.senderAdress, mail.receiverAdress, mail.content);
-
-        //if (!contentImageText.activeInHierarchy)
-        //{
-        //    contentImageText.SetActive(true);
-        //}
-
-        //TMP_Text contentSubject = contentImageText.transform.GetChild(0).GetComponent<TMP_Text>();
-        //TMP_Text contentDate = contentImageText.transform.GetChild(1).GetComponent<TMP_Text>();
-        //TMP_Text contentFrom = contentImageText.transform.GetChild(2).GetComponent<TMP_Text>();
-        //TMP_Text contentTo = contentImageText.transform.GetChild(3).GetComponent<TMP_Text>();
-        //TMP_Text contentMail = contentImageText.transform.GetChild(4).GetComponent<TMP_Text>();
-
-        //contentSubject.text = mail.subject;
-        //contentDate.text = mail.date;
-        //contentFrom.text = mail.senderAdress;
-        //contentTo.text = mail.receiverAdress;
-        //contentMail.text = mail.content;
-
         Debug.Log("Mail Subject of Button clicked: " + mail.subject);
     }
 }
