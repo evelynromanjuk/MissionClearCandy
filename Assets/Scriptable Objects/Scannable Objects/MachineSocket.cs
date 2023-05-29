0c97bce60e43c3b5d7e2bd3b9226b4e017a649d6
Evelyn <evelyn.romanjuk@gmail.com>
2023-05-29 22:18:43 +0200

    Added scannable sockets for robot

---
+++ Assets/Scriptable Objects/Scannable Objects/MachineSocket.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New MachineSocket", menuName = "MachineSocket")]
+public class MachineSocket : ScannableData
+{
+    public SecurityElement MachinePart;
+
+    private string[,] _socketData;
+
+
+    public override string[,] GetScanData { get => _socketData; }
+
+    public override void CreateArray()
+    {
+        if (_socketData == null & MachinePart != null)
+        {
+            _socketData = new string[,] { { "objectType", objectType }, { "description", MachinePart.name }, { "status", MachinePart.Status } };
+        }
+    }
+
+    public override void Reset()
+    {
+        objectType = "machine socket";
+    }
+
+}
