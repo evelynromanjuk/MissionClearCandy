142d42faf05867df35dbdc5e1b557380a1af86d6
Evelyn <evelyn.romanjuk@gmail.com>
2023-06-03 17:11:59 +0200

    Added alarm and emergency alert countdown, Turn Off button

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -2,35 +2,110 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Alarm : MonoBehaviour
 {
-    Action AlarmStarted;
-    Action AlarmPaused;
+    public TMP_Text AlarmText;
+    public TMP_Text EmergencyAlert;
+    public TMP_Text EmergencyText;
+
+    public Image AlarmIcon;
+
+    private bool _alarmIsRunning;
+    private float _alarmTimeRemaining = 10f;
+
+    private bool _emergencyIsRunning;
+    private float _emergencyTimeRemaining = 15f;
 
     void Start()
     {
-        InvokeRepeating("StartAlarm", 10, 0);
+        _alarmIsRunning = true;
+        _emergencyIsRunning = false;
+    }
+
+    private void Update()
+    {
+        if(_alarmIsRunning)
+        {
+            CountdownAlarm();
+        }
+        if(_emergencyIsRunning)
+        {
+            CountdownEmergency();
+        }
     }
 
     void StartAlarm()
     {
         AudioManager.Instance.Play("Alarm");
+
+        AlarmText.gameObject.SetActive(false);
+        EmergencyAlert.gameObject.SetActive(true);
+
+        AlarmIcon.gameObject.SetActive(true);
+
+        _emergencyIsRunning = true;
     }
 
-    public void PauseAlarm()
+    public void ResetAlarm()
     {
         AudioManager.Instance.Stop("Alarm");
-        InvokeRepeating("StartAlarm", 60, 0);
+
+        AlarmText.gameObject.SetActive(true);
+        EmergencyAlert.gameObject.SetActive(false);
+
+        AlarmIcon.gameObject.SetActive(false);
+
+        _alarmTimeRemaining = 10f;
+        _alarmIsRunning = true;
+
+        _emergencyTimeRemaining = 15f;
     }
 
-    public void SubscribeAlarmStarted(Action method)
+    private void CountdownAlarm()
     {
-        AlarmStarted += method;
+        if (_alarmTimeRemaining > 0)
+        {
+            _alarmTimeRemaining -= Time.deltaTime;
+            ChangeAlarmText();
+        }
+        else
+        {
+            _alarmTimeRemaining = 0;
+            ChangeAlarmText();
+            _alarmIsRunning = false;
+            StartAlarm();
+        }
+    }
+    
+    private void CountdownEmergency()
+    {
+        if (_emergencyTimeRemaining > 0)
+        {
+            _emergencyTimeRemaining -= Time.deltaTime;
+            ChangeEmergencyText();
+        }
+        else
+        {
+            _emergencyTimeRemaining = 0;
+            ChangeEmergencyText();
+            _emergencyIsRunning = false;
+            // TO DO: GAME OVER screen or smth when emergency timer is done
+        }
     }
 
-    public void SubscribeAlarmPaused(Action method)
+    private void ChangeAlarmText()
     {
-        AlarmPaused += method;
+        int TimeInSeconds = Mathf.RoundToInt(_alarmTimeRemaining);
+        AlarmText.text = ">>> Next alarm in " + TimeInSeconds + " seconds";
     }
+
+    private void ChangeEmergencyText()
+    {
+        int TimeInSeconds = Mathf.RoundToInt(_emergencyTimeRemaining);
+        EmergencyText.text = ">>> Emergency shutdown in " + TimeInSeconds + " seconds";
+    }
+
 }
--- Assets/Scripts/VR Interactions/OpenCloseFurniture.cs
+++ Assets/Scripts/VR Interactions/OpenCloseFurniture.cs
@@ -7,7 +7,21 @@ public class OpenCloseFurniture : MonoBehaviour
     [SerializeField] private Animator _interactableObject = null;
 
     public bool isOpen = false;
+    public string OpeningSoundName;
+    public string ClosingSoundName;
 
+    private void Start()
+    {
+        if(OpeningSoundName == null)
+        {
+            Debug.Log("OpeningSoundName for " + this.gameObject + " not set.");
+        }
+
+        if (ClosingSoundName == null)
+        {
+            Debug.Log("ClosingSoundName for " + this.gameObject + " not set.");
+        }
+    }
 
     public void SetActive()
     {
@@ -15,11 +29,13 @@ public class OpenCloseFurniture : MonoBehaviour
         if (!isOpen)
         {
             _interactableObject.Play("Open", 0, 0.0f);
+            AudioManager.Instance.Play(OpeningSoundName);
             isOpen = true;
         }
         else
         {
             _interactableObject.Play("Close", 0, 0.0f);
+            AudioManager.Instance.Play(ClosingSoundName);
             isOpen = false;
         }
     }
